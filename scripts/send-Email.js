6f0db47b2d170e870373b8ea9a2327d44a46767c
gama-A <gamaliel@ucsb.edu>
2023-10-25 14:18:26 -0700

    updates to contact form

--- scripts/send-Email.js
+++ scripts/send-Email.js
@@ -1,32 +1,31 @@
 function sendEmail() {
     var name =  document.getElementById('name').value;
     if (name == "") {
-        document.querySelector('.status').innerHTML = "Name cannot be empty";
+        document.getElementById('status').textContent = "NAME CANNOT BE EMPTY";
         return false;
     }
     var email =  document.getElementById('email').value;
     if (email == "") {
-        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        document.getElementById('status').textContent = "EMAIL CANNOT BE EMPTY";
         return false;
     } else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if(!re.test(email)){
-            document.querySelector('.status').innerHTML = "Email format invalid";
+            document.getElementById('status').textContent = "EMAIL FORMAT INVALID";
             return false;
         }
     }
     var subject =  document.getElementById('subject').value;
     if (subject == "") {
-        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        document.getElementById('status').textContent = "SUBJECT CANNOT BE BLANK";
         return false;
     }
     var message =  document.getElementById('message').value;
     if (message == "") {
-        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        document.getElementById('status').textContent = "MESSAGE CANNOT BE BLANK";
         return false;
     }
-    //document.querySelector('.status').innerHTML = "Sending...";
-
+    
     var params = {
         name: name,
         email: email,
@@ -44,6 +43,7 @@ function sendEmail() {
             document.getElementById("subject").value = "";
             document.getElementById("message").value = "";
             console.log(res);
+            document.getElementById('status').value = "";
             alert("message sent successfully");
         }
     ).catch((err) => console.log(err));
