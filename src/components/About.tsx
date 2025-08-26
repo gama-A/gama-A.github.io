2b4a7e6c2250581af76fbf18b7fdb9711058ce7f
gama-A <59851451+gama-A@users.noreply.github.com>
2025-08-25 23:47:33 -0700

    added images and linkedin

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,19 @@
-import { Typography } from "@mui/material";
+import { Avatar, Typography } from "@mui/material";
+import Muilink from '@mui/material/Link';
 
 export function AboutComponent() {
     return (
         <Typography component="div" variant="body1" align="left">
             This page is intended to demonstrate both my skills as a programmer and my other special interests. <br />
-            Feel free to explore the tabs to see my work and experience.<br/>Contact me: <a href="mailto:gama.aristondo@gmail.com">gama@aristondo@gmail.com</a>
+            Feel free to explore the tabs to see my work and experience.<br/>Contact me:<br/>
+            <Muilink href="https://www.linkedin.com/in/gama-aristondo-634a80210/">
+                <Avatar
+                    alt="linkedin"
+                    src="/src/assets/in-logo/InBug-Black.png"
+                    variant="square"
+                />
+            </Muilink>
+            <Muilink href="mailto:gama.aristondo@gmail.com" color="secondary">gama.aristondo@gmail.com</Muilink>
         </Typography>
     );
 }
