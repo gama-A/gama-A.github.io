567876d99e733d89b3a0dc3ea890f80d12501288
gama <gama.aristondo@gmail.com>
2025-08-30 10:39:38 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
                       <Muilink href="https://www.linkedin.com/in/gama-aristondo-634a80210/" underline='hover'>
                           <Avatar
                               alt="linkedin"
-                              src="/src/assets/InBug-White.png"
+                              src="/assets/InBug-White.png"
                               variant="square"
                           />
                       </Muilink>
